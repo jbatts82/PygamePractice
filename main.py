6c7dfdc162cb9bdfa2818e476fe4c5b25819c585
jbatts82 <jbatts82@hotmail.com>
2022-11-25 13:18:09 -0500

    framework

--- main.py
+++ main.py
@@ -4,8 +4,7 @@
 # Description: Main Loop
 ###############################################################################
 
-print("Hello, Main Loop")
-
+import sys
 import pygame
 import tools.spriteSheet
 import data.constants as const
@@ -18,15 +17,27 @@ pygame.display.set_caption('Sprite Lab')
 sprite_sheet_loc = 'resources/graphics/mario_bros.png'
 sprite_sheet_image = pygame.image.load(sprite_sheet_loc).convert_alpha()
 
+# extract animations from sprite sheet
+mario_animations = []
+mario_steps = [3]
+mario_animation_num = 0
+
 # set up clock
 clock = pygame.time.Clock()
 
 run = True
-while run:
-    screen.fill(const.BG)
 
+while run:
+
+    screen.fill(const.BG)
 
     # Event Handler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    clock.tick(const.FPS)
+
+# if not run
+pygame.quit()
+sys.exit()
