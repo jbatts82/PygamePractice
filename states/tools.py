80690339babed685214b01cce73a2891b35b119e
jbatts82 <jbatts82@hotmail.com>
2022-11-27 07:25:16 -0500

    adding tools file. will store helper functions here.

---
+++ states/tools.py
@@ -0,0 +1,17 @@
+###############################################################################
+# File Name  : tools.py
+# Date       : 11/27/2022
+# Description: System control functions. Helper functions.
+###############################################################################
+
+import os
+import pygame
+
+keybindings = {
+    'action': pygame.K_s,
+    'jump': pygame.K_a,
+    'left': pygame.K_LEFT,
+    'right': pygame.K_RIGHT,
+    'down': pygame.K_DOWN
+}
+
